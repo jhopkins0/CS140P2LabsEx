3119b5962ff10822820d30559dff4765d83ed8b2
Jordan J Hopkins <jhopkins4@students.avc.edu>
2025-05-01 12:20:30 -0700

    ex1 entry

--- ex1.asm
+++ ex1.asm
@@ -1,6 +1,29 @@
 .data
-
+    g: .word 100
+    h: .word 200
+    ival: .word 10
+    jval: .word 20
+    fval: .word 0
 .text
 .globl main
 main:
 
+    lw $t1, g
+    lw $t2, h
+    lw $t3, ival
+    lw $t4, jval
+    
+    bne $t3, $t4, false
+    add $t0. $t3, $t4
+    j endif
+false:
+    sub $t0, $t3, $t4
+
+endif:
+    sw $t0, false
+    li $v0, 1
+    move $a0, $t0
+    syscall
+
+    li $v0, 10
+    syscall
