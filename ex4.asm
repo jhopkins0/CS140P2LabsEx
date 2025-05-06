fc7612ff0b634eeb258caa8ffd89b09b56f38274
Jordan J Hopkins <jhopkins4@students.avc.edu>
2025-05-06 12:10:52 -0700

    change 4

--- ex4.asm
+++ ex4.asm
@@ -13,24 +13,16 @@ main:
     ori $t3, $zero, 0
     ori $t4, $zero, 1
 
+    ori $t7, $t0, 0
 loop:
     lw $t7, 0($t0)
     add $t1, $t1, $t7
 
     add $t3, $t3, $t4
-    
+    sll $t7, $t3, 2
+    add $t7, $t7, $t0
 
     bne $t3, $t2, loop
-
-    add $t1, $t1, $t7
-
-    add $t7, $t0, 4
-    lw $t7, 0($t0+4)
-    add $t1, $t1, $t7
-
-    #lui $t0, 0x1001
-    #ori $t0, $t0, 0004
-
 
     li $v0, 1
     move $a0, $t0
