f07ec5714071fb05b45c6b734895d4e5ff8b5d30
Jordan <jhopkins9@live.com>
2025-05-06 22:39:06 -0700

    add ex6

---
+++ ex6.asm
@@ -0,0 +1,36 @@
+.data
+    A:      .word   5, 10, 25, 35, 45
+    sum:    .word   30
+    idx:    .word   0
+.text
+.globl main
+
+main:
+
+    li  $t1, 0
+    li  $t2, 10
+    ori $t3, 0
+    
+loop:
+    slt $t4, $t1, $t2
+    beq $t4, $zero, exit
+
+    sll $t5,  $t1,  2
+    add $t5, $t5, $t0
+    lw $t4, 0($t5)
+    add $t3, $t3, $t4
+    
+    addi $ti, $t1, 1
+    j loop
+
+
+
+exit:   
+    sw $t1, idx
+    li $v0, 1
+    move $a0, $t0
+    syscall
+
+    li $v0, 10
+    syscall
+
